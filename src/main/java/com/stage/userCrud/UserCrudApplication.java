2543a4bddbf7f43a1633d061ee0dc8127c753f91
chayma <chaymariahi872@gmail.com>
2023-08-27 17:54:22 +0100

    Premier commit

--- src/main/java/com/stage/userCrud/UserCrudApplication.java
+++ src/main/java/com/stage/userCrud/UserCrudApplication.java
@@ -2,7 +2,6 @@ package com.stage.userCrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class UserCrudApplication {
 
